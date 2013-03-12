d8f68966f8fd534b01e8615755e99be0734200e2
R-Y <k_nazuki@hkg.odn.ne.jp>
2013-03-12 16:03:28 +0900

    jsonパース

--- lib/poe-tools.rb
+++ lib/poe-tools.rb
@@ -1,10 +1,11 @@
 # -*- encoding: utf-8 -*-
 
-#require 'json'
-require 'rsvg2'
+require 'json'
+#require 'rsvg2'
 
 #rsvg-convert -w 200 smile_only.svg -o testes.png
 
+=begin ===========================================================================
 SRC = "./images/svg/smile_only.svg"
 DST = "smile_only.png"
 
@@ -23,7 +24,26 @@ context = Cairo::Context.new(surface)
 context.scale(ratio_w, ratio_h)
 context.render_rsvg_handle(handle)
 surface.write_to_png(DST)
+=end ===========================================================================
 
 # #File.open('index.json", r
 #
 # #"images/svg/???.svg" -> "images/png64/???.png"
+
+filenames = Array.new
+
+file = open('index.json').read
+data = JSON.parse(file)
+data.each do |list|
+  puts list['moji']
+  puts list['name']
+  puts list['name-ja']
+  puts list['unicode']
+  filename = 'images/svg/' + list['name'] + '.svg'
+  filenames.push(filename)
+end
+
+filenames.each do |name|
+  puts name
+  # ここでコンバートとか…？
+end
