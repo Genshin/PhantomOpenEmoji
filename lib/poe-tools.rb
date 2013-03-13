95316cf508ec131ca45448600393dd456dea742c
影月 零 <zero@genshin.org>
2013-03-13 16:16:53 +0900

    無月さんにタスクの骨組み

--- lib/poe-tools.rb
+++ lib/poe-tools.rb
@@ -1,49 +1,23 @@
 # -*- encoding: utf-8 -*-
 
-require 'json'
-#require 'rsvg2'
+require './poe.rb'
 
-#rsvg-convert -w 200 smile_only.svg -o testes.png
+poe = POE.new
 
-=begin ===========================================================================
-SRC = "./images/svg/smile_only.svg"
-DST = "smile_only.png"
+#TODO 
+# $ poe-tools -h
+# $ poe-tools help
+# * print help
 
-handle = RSVG::Handle.new_from_file(SRC)
+#TODO
+# $ poe-tools -i {index_file}
+# * the -i flag allows specification of an index other than the default
 
-ratio = 1.0
+#TODO 
+# $ poe-tools convert {format} {px}
+# E poe-tools convert png 64
+# * converts to {format} in {px} size
+# - defaults to png when format not specified
+# - defaults to 64 when size not specified
 
-output_px = 128
-
-dim = handle.dimensions
-ratio_w = output_px.to_f / dim.width.to_f
-ratio_h = output_px.to_f / dim.height.to_f
-
-surface = Cairo::ImageSurface.new(Cairo::FORMAT_ARGB32, output_px, output_px)
-context = Cairo::Context.new(surface)
-context.scale(ratio_w, ratio_h)
-context.render_rsvg_handle(handle)
-surface.write_to_png(DST)
-=end ===========================================================================
-
-# #File.open('index.json", r
-#
-# #"images/svg/???.svg" -> "images/png64/???.png"
-
-filenames = Array.new
-
-file = open('index.json').read
-data = JSON.parse(file)
-data.each do |list|
-  puts list['moji']
-  puts list['name']
-  puts list['name-ja']
-  puts list['unicode']
-  filename = 'images/svg/' + list['name'] + '.svg'
-  filenames.push(filename)
-end
-
-filenames.each do |name|
-  puts name
-  # ここでコンバートとか…？
-end
+poe.parse_json_index('index.json')
