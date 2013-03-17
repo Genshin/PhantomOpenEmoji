c1c9b66ac21fd52c3d5f1c8e71dd090af53f3c3a
影月 零 <zero@genshin.org>
2013-03-18 00:38:52 +0900

    cleaning

--- lib/poe-tools.rb
+++ lib/poe-tools.rb
@@ -4,83 +4,97 @@
 require './poe.rb'
 require 'optparse'
 
-poe = POE.new
+@poe = POE.new
+@format = 'png'
+@size = 64
 
-#TODO
-# $ poe-tools convert {format} {px}
-# E poe-tools convert png 64
-# * converts to {format} in {px} size
-# - defaults to png when format not specified
-# - defaults to 64 when size not specified
+def list_emoji()
+  index = @poe.get_index
 
-# helpを表示
-if ARGV[0] == 'help'
-  puts poe.print_help
+  index.each do |item|
+    str = ""
+    if !(item['moji'].nil?)
+      str += item['moji']
+    end
+    str += "\t"
+    if !(item['name'].nil?)
+      str += item['name']
+    end
+    str += "\t"
+    if !(item['name-ja'].nil?)
+      str += item['name-ja']
+    end
+    str += "\t"
+    if !(item['unicode'].nil?)
+      str += item['unicode']
+    end
+    puts str
+  end
 end
+
 
 opts = OptionParser.new
-# helpを表示
-opts.on('-h', '--help') {|v|
-  poe.print_help
-}
 # 一覧表示
-opts.on('-l') {|v|
-  poe.show_items
+opts.on('--list', '-l') {|v|
+  list_emoji
 }
+
 # ./index.json以外のファイル使用
-opts.on('-i=FILE') {|v|
-  poe.parse_json_index(v)
+opts.on('-i', '--index') {|index_file|
+  @poe.parse_json_index(index_file)
 }
-# コンバート
-opts.on('-c=FORMAT PX', '--convert=FORMAT PX') {|v|
-  if ARGV[0].nil?
-    puts 'error'
-  elsif v == 'png'
-    poe.all_to_png(ARGV[0])
-  end
-  # 将来的にフォーマットが増える？
+
+opts.on('-f FORMAT', '--format FORMAT') {|format|
+  @format = format
 }
-# 絵文字から名前を検索
-opts.on('--search-name=EMOJI') {|v|
-  poe.emoji_to_name(v)
+
+opts.on('-s PX', '--size PX') {|px|
+  @size = px
 }
-# 絵文字から日本語名を検索
-opts.on('--search-name-ja=EMOJI') {|v|
-  poe.emoji_to_name_ja(v)
-}
-# 名前から絵文字を検索
-opts.on('--search-emoji-name=NAME') {|v|
-  poe.name_to_emoji(v)
-}
-# 日本語名から絵文字を検索
-opts.on('--search-emoji-name-ja=NAME') {|v|
-  poe.name_ja_to_emoji(v)
-}
-opts.parse!(ARGV)
 
 # コンバート
-for num in 0..ARGV.size do
-  if ARGV[num] == 'convert' then
-    if num == ARGV.size - 1 then
-      # デフォルトのコンバート
-      poe.all_to_png(64)
-      # puts 'default'
-      break
-    elsif ARGV[num + 1] != 'png' && ARGV[num + 1] != 'gif' then
-      # {format}が違う
-      puts 'error: poe-tools convert {format} {px}   ex) poe-tools convert png 64'
-      break
-    elsif !(ARGV[num + 2] =~ /[0-9]/)  || ARGV[num + 2].nil? then
-      # {px}が違う
-      puts 'error: poe-tools convert {format} {px}   ex) poe-tools convert png 64'
-      break
-    else
-      if ARGV[num + 1] == 'png'
-        poe.all_to_png(ARGV[num + 2].to_i)
-        # puts 'png' + ARGV[num + 2]
-      end
-      # 将来的に違うフォーマットも増える？
-      break
-    end
+opts.on('-c [OUTDIR]', '--convert [OUTDIR]') {|outdir|
+  if @format == 'png'
+    @poe.index_to_png(@size, outdir)
   end
-end
+}
+
+# 絵文字から検索
+opts.on('--lookup-emoji EMOJI') {|emoji|
+  puts @poe.lookup_emoji(emoji)
+}
+# 名前から検索
+opts.on('--lookup-name NAME') {|name|
+  puts @poe.lookup_name(name)
+}
+# 日本語名から絵文字を検索
+opts.on('--lookup-name-ja NAME') {|name|
+  puts @poe.lookup_name_ja(name)
+}
+
+# for num in 0..ARGV.size do
+#   if ARGV[num] == 'convert' then
+#     if num == ARGV.size - 1 then
+#       # デフォルトのコンバート
+#       poe.all_to_png(64)
+#       # puts 'default'
+#       break
+#     elsif ARGV[num + 1] != 'png' && ARGV[num + 1] != 'gif' then
+#       # {format}が違う
+#       puts 'error: poe-tools convert {format} {px}   ex) poe-tools convert png 64'
+#       break
+#     elsif !(ARGV[num + 2] =~ /[0-9]/)  || ARGV[num + 2].nil? then
+#       # {px}が違う
+#       puts 'error: poe-tools convert {format} {px}   ex) poe-tools convert png 64'
+#       break
+#     else
+#       if ARGV[num + 1] == 'png'
+#         poe.all_to_png(ARGV[num + 2].to_i)
+#         # puts 'png' + ARGV[num + 2]
+#       end
+#       # 将来的に違うフォーマットも増える？
+#       break
+#     end
+#   end
+# end
+opts.parse!(ARGV)
--- lib/poe.rb
+++ lib/poe.rb
@@ -6,42 +6,21 @@ require 'rsvg2'
 class POE
   @index
 
+  def initialize
+    parse_json_index('./index.json')
+  end
+
   def parse_json_index(file)
     file = open(file).read
     @index = JSON.parse(file)
   end
 
-  def show_items()
-    if @index.nil?
-      parse_json_index('./index.json')
-    end
-
-    @index.each do |item|
-      str = ""
-      if !(item['moji'].nil?)
-        str += item['moji']
-      end
-      str += "\t"
-      if !(item['name'].nil?)
-        str += item['name']
-      end
-      str += "\t"
-      if !(item['name-ja'].nil?)
-        str += item['name-ja']
-      end
-      str += "\t"
-      if !(item['unicode'].nil?)
-        str += item['unicode']
-      end
-      puts str
-    end
+  def get_index()
+    return @index
   end
 
-  def all_to_png(px)
-    if @index.nil?
-      parse_json_index('./index.json')
-    end
-
+  def index_to_png(px, outdir)
+    #TODO handle outdir
     @index.each do |item|
       emoji_to_png(item['name'], px)
     end
@@ -66,60 +45,29 @@ class POE
     surface.write_to_png(destination)
   end
 
-  # helpを表示
-  def print_help
-    str = File.read("help.txt", :encoding => Encoding::UTF_8)
-    print(str)
-  end
-
-  # 絵文字から名前を検索
-  def emoji_to_name(emoji)
-    if @index.nil?
-      parse_json_index('./index.json')
-    end
-
+  # 絵文字から検索
+  def lookup_emoji(emoji)
     @index.each do |item|
       if emoji == item['moji']
-        puts item['name']
+        return item
       end
     end
   end
 
-  # 絵文字から日本語名を検索
-  def emoji_to_name_ja(emoji)
-    if @index.nil?
-      parse_json_index('./index.json')
-    end
-
-    @index.each do |item|
-      if emoji == item['moji']
-        puts item['name-ja']
-      end
-    end
-  end
-
-  # 名前から絵文字を検索
-  def name_to_emoji(name)
-    if @index.nil?
-      parse_json_index('./index.json')
-    end
-
+  # 名前から検索
+  def lookup_name(name)
     @index.each do |item|
       if name == item['name']
-        puts item['moji']
+        return item
       end
     end
   end
 
-  # 日本語名から絵文字を検索
-  def name_ja_to_emoji(name_ja)
-    if @index.nil?
-      parse_json_index('./index.json')
-    end
-
+  # 日本語名から検索
+  def lookup_name_ja(name_ja)
     @index.each do |item|
       if name_ja == item['name-ja']
-        puts item['moji']
+        return item
       end
     end
   end
